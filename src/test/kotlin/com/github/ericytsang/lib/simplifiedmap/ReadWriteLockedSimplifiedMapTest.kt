6a5093ff4ca8b53240354f351f0de990cefe5966
Eric Tsang <surplus.et@gmail.com>
2017-01-03 07:01:21 -0800

    added unit tests
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ src/test/kotlin/com/github/ericytsang/lib/simplifiedmap/ReadWriteLockedSimplifiedMapTest.kt
@@ -0,0 +1,93 @@
+package com.github.ericytsang.lib.simplifiedmap
+
+import org.junit.Test
+import kotlin.concurrent.write
+
+class ReadWriteLockedSimplifiedMapTest
+{
+    val map = mutableMapOf("a" to 3,"b" to 4)
+        .let {SimplifiedMapWrapper(it)}
+        .let {ReadWriteLockedSimplifiedMapWrapper(it)}
+
+    @Test
+    fun getTest()
+    {
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+    }
+
+    @Test
+    fun removeTestWithLock()
+    {
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+        map.readWriteLock.write {map["b"] = null}
+        assert(map["a"] == 3)
+        assert(map["b"] == null)
+        assert(map["c"] == null)
+    }
+
+    @Test
+    fun setTestWithLock()
+    {
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+        map.readWriteLock.write {map["b"] = 7}
+        assert(map["a"] == 3)
+        assert(map["b"] == 7)
+        assert(map["c"] == null)
+    }
+
+    @Test
+    fun removeTestNoLock()
+    {
+        // check state
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+
+        // try to manipulate map
+        try
+        {
+            map["b"] = null
+            assert(false)
+        }
+        catch (ex:IllegalStateException)
+        {
+            // ignore exception
+        }
+
+        // state should not have changed
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+    }
+
+    @Test
+    fun setTestNoLock()
+    {
+        // check state
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+
+        // try to manipulate map
+        try
+        {
+            map["b"] = 7
+            assert(false)
+        }
+        catch (ex:IllegalStateException)
+        {
+            // ignore exception
+        }
+
+        // state should not have changed
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+    }
+}
---
+++ src/test/kotlin/com/github/ericytsang/lib/simplifiedmap/SimplifiedMapWrapperTest.kt
@@ -0,0 +1,41 @@
+package com.github.ericytsang.lib.simplifiedmap
+
+import org.junit.Test
+
+class SimplifiedMapWrapperTest
+{
+    val map = mutableMapOf("a" to 3,"b" to 4)
+        .let {SimplifiedMapWrapper(it)}
+
+    @Test
+    fun getTest()
+    {
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+    }
+
+    @Test
+    fun removeTest()
+    {
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+        map["b"] = null
+        assert(map["a"] == 3)
+        assert(map["b"] == null)
+        assert(map["c"] == null)
+    }
+
+    @Test
+    fun setTest()
+    {
+        assert(map["a"] == 3)
+        assert(map["b"] == 4)
+        assert(map["c"] == null)
+        map["b"] = 7
+        assert(map["a"] == 3)
+        assert(map["b"] == 7)
+        assert(map["c"] == null)
+    }
+}
