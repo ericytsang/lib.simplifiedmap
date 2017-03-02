c21bdde4d3c2b00fff80d99903484dff44085ab4
Eric Tsang <surplus.et@gmail.com>
2017-03-02 08:21:32 -0800

    added more documentation

--- src/main/kotlin/com/github/ericytsang/lib/simplifiedmap/SimplifiedMap.kt
+++ src/main/kotlin/com/github/ericytsang/lib/simplifiedmap/SimplifiedMap.kt
@@ -4,7 +4,26 @@ import java.util.AbstractMap
 
 interface SimplifiedMap<K,V:Any>:Map<K,V>
 {
+    /**
+     * @param key key in the map to remap to [value].
+     * @param value if null, remove the entry such that [get] ([key]) == null.
+     * else, remap [key] to [value] such that [get] ([key]) == [value].
+     * @return the value that was previously associated with [key] or null if
+     * the entry did not previously exist.
+     */
     operator fun set(key:K,value:V?):V?
+
+    /**
+     * @param key the key associated with the [V] to return.
+     * @return the value that is associated with [key] or null if the entry
+     * doesn't exist.
+     */
+    override operator fun get(key:K):V?
+
+    /**
+     * set of all [K]s that are associated with a [V].
+     */
+    override val keys:Set<K>
 
     override val size:Int get() = keys.size
 
